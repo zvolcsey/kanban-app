85ab0c5eaed496ac3b5cf210520bd9714a33d856
Zoltán Völcsey <41576384+zvolcsey@users.noreply.github.com>
2026-03-10 12:00:23 +0100

    chore(root): add eslint cofiguration

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  globalIgnores(["**/dist/**", "*/node_modules/**", "**/build/**"]),
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["apps/client/**/*.{ts,tsx}"],
+    extends: [reactHooks.configs.flat.recommended, reactRefresh.configs.vite],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+  {
+    files: ["apps/server/**/*.ts"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  {
+    files: ["packages/shared/**/*.ts"],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+]);
